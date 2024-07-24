d20f14e89e4574070dd2e4d80d70fc146300435d
Akshat <ad.akshat09@gmail.com>
2024-07-24 22:09:43 +0530

    second commit

--- backend/utils/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const dbURI = 'mongodb+srv://akshat123:qwerty123@real-estate.21f1utk.mongodb.net/?retryWrites=true&w=majority&appName=real-estate';
-
-mongoose.connect(dbURI, {})
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.log('MongoDB connection error: ', err));
-
-export default mongoose;
